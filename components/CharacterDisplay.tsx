4da9e7232c9f6fea93b451198b5df7b3cae40e0d
Djobbo <hi@djobbo.com>
2024-04-08 22:26:41 +0200

    characterDisplay animation

--- components/CharacterDisplay.tsx
+++ components/CharacterDisplay.tsx
@@ -1,3 +1,5 @@
+import { AnimatePresence, motion } from "framer-motion"
+
 type CharacterDisplayProps = {
     character: string
     pronounciation?: string
@@ -16,10 +18,42 @@ export const CharacterDisplay = ({
                 viewBox="0 0 320 320"
                 fill="none"
                 xmlns="http://www.w3.org/2000/svg"
+                overflow="visible"
             >
-                <circle cx="160" cy="160" r="136" className="fill-card" />
+                <motion.circle
+                    cx="160"
+                    cy="160"
+                    r="136"
+                    className="fill-card"
+                    initial={{ scale: 0.75, opacity: 0 }}
+                    animate={{ scale: 1, opacity: 1 }}
+                    exit={{ scale: 0, opacity: 0 }}
+                    transition={{
+                        scale: {
+                            type: "spring",
+                            damping: 12,
+                            stiffness: 400,
+                            restDelta: 0.01,
+                        },
+                        duration: 0.2,
+                    }}
+                />
                 {pronounciation && (
-                    <g>
+                    <motion.g
+                        initial={{ scale: 0.8, opacity: 0 }}
+                        animate={{ scale: 1, opacity: 1 }}
+                        exit={{ scale: 0.8, opacity: 0 }}
+                        transition={{
+                            scale: {
+                                type: "spring",
+                                damping: 5,
+                                stiffness: 350,
+                                restDelta: 0.01,
+                            },
+                            delay: 0.2,
+                            duration: 0.08,
+                        }}
+                    >
                         <circle
                             cx="256"
                             cy="64"
@@ -35,26 +69,45 @@ export const CharacterDisplay = ({
                         >
                             {pronounciation}
                         </text>
-                    </g>
+                    </motion.g>
                 )}
-                <line
+                <motion.line
                     x1="160"
                     y1="0"
                     x2="160"
-                    y2="320"
-                    className="stroke-accent/50 stroke-1 svg-stroke-dashed "
+                    className="stroke-accent/50 stroke-1 svg-stroke-dashed"
+                    initial={{ y1: 160, y2: 160 }}
+                    animate={{ y1: 0, y2: 320 }}
+                    exit={{ y1: 160, y2: 160 }}
+                    transition={{
+                        duration: 0.2,
+                        ease: "easeOut",
+                    }}
                 />
-                <line
-                    x1="0"
+                <motion.line
                     y1="160"
-                    x2="320"
                     y2="160"
-                    className="stroke-accent/50 stroke-1 svg-stroke-dashed "
+                    className="stroke-accent/50 stroke-1 svg-stroke-dashed"
+                    initial={{ x1: 160, x2: 160 }}
+                    animate={{ x1: 0, x2: 320 }}
+                    exit={{ x1: 160, x2: 160 }}
+                    transition={{
+                        duration: 0.2,
+                        ease: "easeOut",
+                    }}
                 />
             </svg>
-            <span className="absolute inset-0 flex justify-center items-center text-9xl">
+            <motion.span
+                className="absolute inset-0 flex justify-center items-center text-9xl"
+                initial={{ scale: 1.25, opacity: 0 }}
+                animate={{ scale: 1, opacity: 1 }}
+                exit={{ scale: 1.25, opacity: 0 }}
+                transition={{
+                    duration: 0.1,
+                }}
+            >
                 {character}
-            </span>
+            </motion.span>
         </div>
     )
 }
