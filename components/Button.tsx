92bcb872345c098a0292751032833a4fdaa5cfac
Djobbo <hi@djobbo.com>
2024-04-08 21:54:21 +0200

    add framer & clsx

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,3 +1,4 @@
+import { cn } from "@/util/cn"
 import { ButtonHTMLAttributes } from "react"
 
 type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>
@@ -5,7 +6,10 @@ type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>
 export const Button = ({ className, ...props }: ButtonProps) => {
     return (
         <div
-            className={`bg-primary mt-4 rounded-2xl flex items-center pb-1 ${className}`}
+            className={cn(
+                "bg-primary mt-4 rounded-2xl flex items-center pb-1",
+                className,
+            )}
         >
             <button
                 className="flex-1 bg-primaryLight hover:bg-primary text-white rounded-2xl p-2 text-bg"
